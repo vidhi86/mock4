905eea81da168280600da495fe2718cf99e1fed9
Vidhi Sharma <vidhisharma@192.168.1.106>
2023-05-27 02:09:45 +0530

    done

---
+++ server.js
@@ -0,0 +1,13 @@
+const jsonServer = require("json-server");
+const server = jsonServer.create();
+const router = jsonServer.router("db.json");
+const middlewares = jsonServer.defaults();
+const cors = require("cors");
+
+server.use(cors());
+server.use(middlewares);
+server.use("", router);
+
+server.listen(process.env.PORT || 5000, () => {
+  console.log("JSON Server is running");
+});
